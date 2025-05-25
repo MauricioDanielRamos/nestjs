ccec2713c9f12b40f462d47344c6de40a3c29870
Mauricio <claum_55@hotmail.com>
2025-05-24 22:23:52 -0300

    se agregan archivos html

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import e from 'express';
+import { title } from 'process';
+
+export interface iTrack {
+  id: number;
+  title: string;
+  duracion:number;
+  artist: string
+}
+
+export const tracks: iTrack[] = [
+  {id: 1,
+  title: "cancion 1",
+  duracion: 120,
+  artist: "inteprete 1" },
+  {id: 1,
+  title: "cancion 2",
+  duracion: 120,
+  artist: "inteprete 2" },
+  {id: 1,
+  title: "cancion 3",
+  duracion: 120,
+  artist: "inteprete 3" },
+]
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getTracks(): iTrack[] {
+    return tracks;
   }
 }
